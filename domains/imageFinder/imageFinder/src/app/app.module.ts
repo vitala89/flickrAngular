6fed9585533746cfcfe75c35b814dc67c1524c37
Vitalii <60993299+vitala89@users.noreply.github.com>
2021-03-20 19:06:58 +0200

    image finder flickr

--- domains/imageFinder/imageFinder/src/app/app.module.ts
+++ domains/imageFinder/imageFinder/src/app/app.module.ts
@@ -1,12 +1,12 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import {ButtonModule} from "primeng/button";
 import {InputTextModule} from "primeng/inputtext";
-import {MainLayoutComponent} from './main-layout/main-layout.component';
-import {ImageListComponent} from './image-list/image-list.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { ImageListComponent } from './image-list/image-list.component';
 import {MenubarModule} from "primeng/menubar";
 import {SharedModule} from "primeng/api";
 import {AvatarModule} from "primeng/avatar";
@@ -16,12 +16,13 @@ import {HttpClientModule} from "@angular/common/http";
 import {ChipsModule} from "primeng/chips";
 import {PaginatorModule} from "primeng/paginator";
 import {DataViewModule} from "primeng/dataview";
-import {BookmarksComponent} from './bookmarks/bookmarks.component';
-import {TruncatePipe} from './pipes/truncate.pipe';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { TruncatePipe } from './pipes/truncate.pipe';
 import {ToastModule} from "primeng/toast";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {BadgeModule} from "primeng/badge";
-
+import { FacebookLoginProvider, SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {RippleModule} from "primeng/ripple";
 @NgModule({
   declarations: [
     AppComponent,
@@ -47,10 +48,26 @@ import {BadgeModule} from "primeng/badge";
     DataViewModule,
     ToastModule,
     BrowserAnimationsModule,
-    BadgeModule
+    BadgeModule,
+    SocialLoginModule,
+    RippleModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider(
+              '274554990533251'
+            )
+          }
+        ]
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
--- domains/imageFinder/imageFinder/src/app/main-layout/main-layout.component.ts
+++ domains/imageFinder/imageFinder/src/app/main-layout/main-layout.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, HostListener, OnInit} from '@angular/core';
+import { SocialAuthService, FacebookLoginProvider, SocialUser } from 'angularx-social-login';
+import { Subject } from 'rxjs';
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss']
 })
 export class MainLayoutComponent implements OnInit {
+  userActivity: any;
+  userInactive: Subject<any> = new Subject();
+  socialUser: SocialUser;
+  isLoggedin: boolean = false;
+  constructor(private socialAuthService: SocialAuthService) {
+    this.setTimeout();
+    this.userInactive.subscribe(() => alert('User inactive one minute'));
+  }
 
-  constructor() { }
+  ngOnInit() {
+  }
+  setTimeout() {
+    this.userActivity = setTimeout(() => this.userInactive.next(this.isLoggedin = false), 60000);
+  }
 
-  ngOnInit(): void {
+  @HostListener('window:mousemove') refreshUserState() {
+    clearTimeout(this.userActivity);
+    this.setTimeout();
+  }
+  loginWithFacebook(): void {
+    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.socialAuthService.signOut();
   }
 
 }
